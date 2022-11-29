33269a91d5d55280d7d17a937225694d34cbada4
jun <jwlee9381@naver.com>
2022-11-29 01:36:39 +0000

    fix

--- template/train.py
+++ template/train.py
@@ -35,20 +35,20 @@ if __name__ == '__main__':
     parser.add_argument('--pooling', default=False, type=bool)
     parser.add_argument('--criterion', default='cross_entropy', type=str)  # cross_entropy, focal_loss
 
-    parser.add_argument('--train_path', default='../dataset/train/removed_paren_train_split.csv')
-    parser.add_argument('--dev_path', default='../dataset/train/val_split.csv')
-    parser.add_argument('--test_path', default='../dataset/train/val_split.csv')
-    parser.add_argument('--predict_path', default='../dataset/test/test_data.csv')
+    parser.add_argument('--train_path', default='/opt/ml/dataset/train/removed_paren_train_split.csv')
+    parser.add_argument('--dev_path', default='/opt/ml/dataset/train/new_val_split.csv')
+    parser.add_argument('--test_path', default='/opt/ml/dataset/train/new_val_split.csv')
+    parser.add_argument('--predict_path', default='/opt/ml/dataset/test/test_data.csv')
     args = parser.parse_args(args=[])
     
-    # try:
-    #     wandb.login(key='4c0a01eaa2bd589d64c5297c5bc806182d126350')
-    # except:
-    #     anony = "must"
-    #     print('If you want to use your W&B account, go to Add-ons -> Secrets and provide your W&B access token. Use the Label name as wandb_api. \nGet your W&B access token from here: https://wandb.ai/authorize')
+    try:
+        wandb.login(key='4c0a01eaa2bd589d64c5297c5bc806182d126350')
+    except:
+        anony = "must"
+        print('If you want to use your W&B account, go to Add-ons -> Secrets and provide your W&B access token. Use the Label name as wandb_api. \nGet your W&B access token from here: https://wandb.ai/authorize')
 
-    # wandb.init(project="level2", name= "4-2. baseline + entity_marker(query) + query augmentation")
-    # wandb_logger = WandbLogger('level2')
+    wandb.init(project="level2", name= "4-2. baseline + entity_marker(query) + query augmentation")
+    wandb_logger = WandbLogger('level2')
 
     dataloader = Dataloader(
         args.tokenizer_name,
@@ -78,7 +78,7 @@ if __name__ == '__main__':
         dirpath="/opt/models/",
         monitor="val_micro_f1",
         save_top_k=-1,
-        filename="roberta-large+{epoch}+{val_micro_f1:.3f}"
+        filename="roberta-large+{epoch}+{val_micro_f1:.3f}",mode='max' 
     )
     lr_monitor = LearningRateMonitor(logging_interval='step')
 
@@ -89,7 +89,7 @@ if __name__ == '__main__':
         log_every_n_steps=1,
         num_sanity_val_steps=0,
         callbacks=[checkpoint_callback],
-        # logger=wandb_logger
+        logger=wandb_logger
     )
 
     # Train part
