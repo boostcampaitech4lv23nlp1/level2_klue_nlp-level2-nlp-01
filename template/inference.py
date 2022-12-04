e9c372f785cb3a009768693daedfef2a8a756d44
이용우 <wooy0ng@iyong-uui-MacBookAir.local>
2022-12-04 15:34:18 +0900

    modify inference.py

--- template/inference.py
+++ template/inference.py
@@ -22,9 +22,7 @@ def weights_update(model, checkpoint):
     return model
 
 if __name__ == '__main__':
-    # 하이퍼 파라미터 등 각종 설정값을 입력받습니다
     # 터미널 실행 예시 : python3 run.py --batch_size=64 ...
-    # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     parser = argparse.ArgumentParser()
     parser.add_argument('--tokenizer_name', default='klue/roberta-large', type=str)
     parser.add_argument('--model_name', default='klue/roberta-large', type=str)
@@ -34,7 +32,7 @@ if __name__ == '__main__':
     
     parser.add_argument('--masking', default=True, type=bool)
     parser.add_argument('--pooling', default=True, type=bool)
-    parser.add_argument('--criterion', default='cross_entropy', type=str)  # cross_entropy, focal_loss
+    parser.add_argument('--criterion', default='cross_entropy', type=str, help='cross_entropy, focal_loss')
 
     parser.add_argument('--train_path', default='../dataset/train/new_train_split.csv')
     parser.add_argument('--dev_path', default='../dataset/train/new_val_split.csv')
