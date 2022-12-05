4bfc657042e5382b318710f40ca254039d244154
이용우 <wooy0ng@172.20.nate.com>
2022-12-05 15:31:13 +0900

    modify inference.py

--- template/inference.py
+++ template/inference.py
@@ -11,9 +11,9 @@ import torchmetrics
 import pytorch_lightning as pl
 
 import matplotlib.pyplot as plt
+
 from dataloader import *
-import wandb
-from pytorch_lightning.loggers import WandbLogger
+
 
 if __name__ == '__main__':
     # 하이퍼 파라미터 등 각종 설정값을 입력받습니다
@@ -22,59 +22,61 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--tokenizer_name', default='klue/roberta-large', type=str)
     parser.add_argument('--model_name', default='klue/roberta-large', type=str)
-
-    parser.add_argument('--batch_size', default=32, type=int)
-    parser.add_argument('--max_epoch', default=5, type=int)
+    parser.add_argument('--batch_size', default=1, type=int)
+    parser.add_argument('--max_epoch', default=1, type=int)
     parser.add_argument('--learning_rate', default=3e-5, type=float)
-    parser.add_argument('--train_path', default='/opt/ml/project2/level2_klue_nlp-level2-nlp-01/dataset/back_translation/cha_train.csv')
-    parser.add_argument('--dev_path', default='/opt/ml/project2/level2_klue_nlp-level2-nlp-01/dataset/back_translation/cha_val.csv')
-    parser.add_argument('--test_path', default='/opt/ml/project2/level2_klue_nlp-level2-nlp-01/dataset/back_translation/cha_val.csv')
-    parser.add_argument('--predict_path', default='/opt/ml/project2/level2_klue_nlp-level2-nlp-01/dataset/test/removed_paren_test_data.csv')
+
+    parser.add_argument('--marker', default=True, type=bool)
+    parser.add_argument('--pooling', default=True, type=bool)
+    parser.add_argument('--criterion', default='cross_entropy', type=str)  # cross_entropy, focal_loss
+    parser.add_argument('--shuffle', default=True, type=bool)
+    
+    parser.add_argument('--k', default=1, type=int)
+    parser.add_argument('--n_folds', default=5, type=bool)
+    parser.add_argument('--split_seed', default=42, type=bool)
+
+    parser.add_argument('--train_path', default='/opt/ml/dataset/train/new_train_split.csv')
+    parser.add_argument('--dev_path', default='/opt/ml/dataset/train/new_val_split.csv')
+    parser.add_argument('--test_path', default='/opt/ml/dataset/train/new_val_split.csv')
+    parser.add_argument('--predict_path', default='/opt/ml/dataset/test/test_data.csv')
     args = parser.parse_args(args=[])
-
-    try:
-        wandb.login(key='1f2c61c98244a7284f6726d8db5d111bca8111fa')
-    except:
-        anony = "must"
-        print('If you want to use your W&B account, go to Add-ons -> Secrets and provide your W&B access token. Use the Label name as wandb_api. \nGet your W&B access token from here: https://wandb.ai/authorize')
-
-
-    wandb.init(project="inference", name= f"{args.model_name}")
-    wandb_logger = WandbLogger('inference')
-
-    dataloader = Dataloader(
-        args.tokenizer_name,
-        args.batch_size,
-        args.train_path,
-        args.dev_path,
-        args.test_path,
-        args.predict_path,
-        shuffle=False
-    )
+    
+    dataloader = Dataloader(args.tokenizer_name, args.batch_size,
+                                args.train_path, args.dev_path, args.test_path, args.predict_path, args.marker, args.shuffle,
+                                args.k, args.n_folds, args.split_seed)
 
     trainer = pl.Trainer(
         accelerator='gpu',
         devices=1,
         precision=16,
         max_epochs=args.max_epoch, 
-        log_every_n_steps=1,
-        logger = wandb_logger
+        log_every_n_steps=1
     )
 
-    model_name = re.sub(r'[/]', '-', args.model_name)
-    model = Model.load_from_checkpoint(checkpoint_path='/opt/ml/template/models/roberta_large(cha_back_trans)+epoch=4+val_micro_f1=91.992.ckpt')
-    results = trainer.predict(model=model, datamodule=dataloader)
+    use_files = []
+    PATH = '/opt/ml/template/kfolds/'  # kfold ckpt 파일 경로
+    for idx, file_nm in enumerate(os.listdir(PATH)):
+        use_files.append(PATH + file_nm)
+    use_files
+    
+    for idx, file_nm in enumerate(use_files):
+        print(f'=======================fold {idx}==========================')
+        model_name = file_nm
+        model = Model.load_from_checkpoint(checkpoint_path=model_name,strict=False)
 
-    preds_all, probs_all = [], []
-    for preds, probs in results:
-        preds_all.append(preds[0]); probs_all.append(str(list(probs)))
+        results = trainer.predict(model=model, datamodule=dataloader)
+    
+        preds_all, probs_all = [], []
+        for preds, probs in results:
+            preds_all.append(preds[0]); probs_all.append(str(list(probs)))
 
-    preds_all = dataloader.num_to_label(preds_all) 
+        preds_all = dataloader.num_to_label(preds_all) 
 
-    output = pd.DataFrame({
-        'id': [idx for idx in range(len(preds_all))],
-        'pred_label': preds_all,
-        'probs': probs_all
-    })
+        output = pd.DataFrame({
+            'id': [idx for idx in range(len(preds_all))],
+            'pred_label': preds_all,
+            'probs': probs_all
+        })
 
-    output.to_csv('/opt/ml/dataset/for_ensemble/output_cha.csv', index=False)
+        output.to_csv(f'/opt/ml/template/kfold_result/kfold_output_{idx}.csv', index=False) # 저장 경로
+        print(f"=======================fold {idx}th DONE=======================")
